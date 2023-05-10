e89a3913666207a534ac8b76c78bc523bf2a4f41
giacomopaolini1 <giacomopaolini08@gmail.com>
2023-05-10 18:14:24 +0200

    feat: html, js

---
+++ js/main.js
@@ -0,0 +1,43 @@
+const {createApp} = Vue
+
+createApp ({
+    data() {
+        return {
+            newTask: "",
+            list: [
+                {
+                    text: "Spesa",
+                    done: false,
+                },
+                {
+                    text: "Pulizia casa",
+                    done: false,
+                },
+                {
+                    text: "Comprare regalo",
+                    done: false,
+                }
+            ]
+        }
+    },
+    methods: {
+        addTask() {
+            this.list.push({
+                text: this.newTask,
+                done: false
+            });
+            this.newTask = "";
+        },
+        completeTask(i) {
+            this.list[i].done = !this.list[i].done;
+        },
+        removeTask(i) {
+            this.list.splice(i, 1);
+        },
+        textDecoration(i) {
+            if (this.list[i].done == true) {
+                return "text-decoration-line-through";
+            }
+        },
+    }
+}).mount('#app')
